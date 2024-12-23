6c8add125b70ada18cbccdf5c105ebdd53b1655b
Leezy <191919qaz@gmail.com>
2024-12-23 22:12:06 +0300

    refactoring cat

--- cat/s21_cat.c
+++ cat/s21_cat.c
@@ -72,36 +72,72 @@ void outline(flags* config, char* line, size_t len) {
 }
 
 void output(flags* config, char* argv[], int argc) {
-  int nonempty_line_count = 0;
-  int line_count = 1;
-  for (int file_index = optind; file_index < argc; file_index++) {
-    FILE* f = fopen(argv[file_index], "r");
-    if (!f) {
-      perror("Error opening file");
-      continue;
+    int nonempty_line_count = 0;
+    int line_count = 1;
+    for (int file_index = optind; file_index < argc; file_index++) {
+        process_single_file(config, argv[file_index], &nonempty_line_count, &line_count);
     }
+}
+#
+void process_single_file(flags* config, char* filename, int* nonempty_line_count, int* line_count) {
+    FILE* f = open_file(filename);
+    if (!f) return;
+    
     char buffer[1024];
     int empty_count = 0;
-    while (fgets(buffer, sizeof(buffer), f)) {
-      size_t len = strlen(buffer);
-      int empty_line = (len == 1 && buffer[0] == '\n');
-      if (empty_line) {
-        empty_count++;
-      } else {
-        empty_count = 0;
-      }
-      if (config->SkipEmptyLines && empty_count > 1) continue;
-      if (config->NumericNonemptyLines && !empty_line) {
-        printf("%6d\t", ++nonempty_line_count);
-        outline(config, buffer, len);
-      } else if (config->NumericAllLines && !config->NumericNonemptyLines) {
-        printf("%6d\t", line_count++);
-        outline(config, buffer, len);
-      } else {
-        outline(config, buffer, len);
-      }
-    }
+    process_file_contents(config, f, buffer, &empty_count, nonempty_line_count, line_count);
     fclose(f);
-  }
 }
 
+FILE* open_file(const char* filename) {
+    FILE* f = fopen(filename, "r");
+    if (!f) {
+        perror("Error opening file");
+    }
+    return f;
+}
+
+void process_file_contents(flags* config, FILE* f, char* buffer, int* empty_count, 
+                         int* nonempty_line_count, int* line_count) {
+    while (fgets(buffer, 1024, f)) {
+        size_t len = strlen(buffer);
+        int empty_line = is_empty_line(buffer, len);
+        
+        update_empty_count(empty_line, empty_count);
+        if (should_skip_line(config, *empty_count)) continue;
+        
+        process_line(config, buffer, len, empty_line, nonempty_line_count, line_count);
+    }
+}
+
+int is_empty_line(const char* buffer, size_t len) {
+    return (len == 1 && buffer[0] == '\n');
+}
+
+void update_empty_count(int empty_line, int* empty_count) {
+    if (empty_line) {
+        (*empty_count)++;
+    } else {
+        *empty_count = 0;
+    }
+}
+
+int should_skip_line(flags* config, int empty_count) {
+    return (config->SkipEmptyLines && empty_count > 1);
+}
+
+void process_line(flags* config, char* buffer, size_t len, int empty_line, 
+                 int* nonempty_line_count, int* line_count) {
+    if (config->NumericNonemptyLines && !empty_line) {
+        print_line_with_number(++(*nonempty_line_count), config, buffer, len);
+    } else if (config->NumericAllLines && !config->NumericNonemptyLines) {
+        print_line_with_number((*line_count)++, config, buffer, len);
+    } else {
+        outline(config, buffer, len);
+    }
+}
+
+void print_line_with_number(int number, flags* config, char* buffer, size_t len) {
+    printf("%6d\t", number);
+    outline(config, buffer, len);
+}
--- cat/s21_cat.h
+++ cat/s21_cat.h
@@ -14,4 +14,15 @@ char flag_v(char ch);
 void outline(flags* arg, char* line, size_t len);
 void output(flags* arg, char* argv[], int argc);
 void FlagsProcces(flags* config, int empty_count, int empty_line, int nonempty_line_count, int line_count, char buffer[], size_t len);
+void proccesfile(flags* config, char* buffer, FILE* f, int empty_count, int nonempty_line_count, int line_count);
+void process_single_file(flags* config, char* filename, int* nonempty_line_count, int* line_count);
+FILE* open_file(const char* filename);
+void process_file_contents(flags* config, FILE* f, char* buffer, int* empty_count, 
+                         int* nonempty_line_count, int* line_count);
+int is_empty_line(const char* buffer, size_t len);
+void update_empty_count(int empty_line, int* empty_count);
+int should_skip_line(flags* config, int empty_count);
+void process_line(flags* config, char* buffer, size_t len, int empty_line, 
+                 int* nonempty_line_count, int* line_count);
+void print_line_with_number(int number, flags* config, char* buffer, size_t len);
 #endif
